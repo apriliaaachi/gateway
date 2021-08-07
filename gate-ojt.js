f97a9f5a7faf35dff8251719b56a9039bfb03368
apriliaaachi <achiaprilia.aa@gmail.com>
2021-08-07 17:04:47 +0700

    update gate-ojt.js

--- gate-ojt.js
+++ gate-ojt.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const router = express();
-const bodyParser = require("body-parser");
-const conf = require("./config/config-port.json");
+const express = require('express')
+const router = express()
+const bodyParser = require('body-parser')
+const conf = require('./config/config-port.json')
 
 /* router */
-const decryption = require("./decryption/app");
+const decryption = require('./decryption/app')
+const encryption = require('./encryption/app')
+const segitiga = require('./segitiga/app')
 
 /* body parser */
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({ extended: true }))
 
 /* use function */
-router.use("/decryption", decryption);
+router.use('/decryption', decryption)
+router.use('/encryption', encryption)
+router.use('/segitiga', segitiga)
 
-console.log("The Gateway on port:" + conf.port);
-router.listen(conf.port, "0.0.0.0");
+console.log('The Gateway on port:' + conf.port);
+router.listen(conf.port, '0.0.0.0');
